7da063da1ef7c9f4d0b91cca776eebddc07aa78a
reiyw <reiyw.setuve@gmail.com>
2018-05-10 19:41:51 +0900

    Ready for challenge

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,33 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+
+
+use rocket_contrib::Json;
+
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[derive(Deserialize)]
+struct Certificate {
+    token: String,
+    challenge: String,
+    #[serde(rename = "type")]
+    typ: String,
+}
+
+#[post("/", format = "application/json", data = "<certificate>")]
+fn verify(certificate: Json<Certificate>) -> String {
+    certificate.0.challenge
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, verify]).launch();
 }
